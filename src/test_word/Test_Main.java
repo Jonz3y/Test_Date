06bd516a62d00f8a4a4c01ab1e8f730ed1360508
Kyle <Kylejones@logisticsalliance.com>
2013-06-06 08:19:58 -0400

    test config

--- src/test_word/Test_Main.java
+++ src/test_word/Test_Main.java
@@ -60,7 +60,7 @@ class TimePrinter implements Runnable  {
                   System.out.println("start to run after interval  : >>> ");
                   FolderFetchIMAP obj = new FolderFetchIMAP();
                   obj.getmail();
-                  Test_WordRead18_truck_date11 importer = new Test_WordRead18_truck_date11();
+                  Test_WordRead18_truck_date11_final importer = new Test_WordRead18_truck_date11_final();
                   importer.CovertFromText();
      
             } catch (Exception ex) {
